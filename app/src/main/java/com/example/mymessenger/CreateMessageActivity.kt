31b52445141127cb787e2b05d99e6add3faab96f
lemmiwinks1551 <ananevka@mail.ru>
2022-04-18 20:21:59 +0300

    implicit intent

--- app/src/main/java/com/example/mymessenger/CreateMessageActivity.kt
+++ app/src/main/java/com/example/mymessenger/CreateMessageActivity.kt
@@ -15,8 +15,10 @@ class CreateMessageActivity : AppCompatActivity() {
 
     //Вызвать onSendMessage() при щелчке на кнопке
     fun onSendMessage(view: View) {
+        /** Вариант создания явного интента*/
+        /*
         // Создать явный интент для запуска активности ReceiveMessage
-        val intent = Intent(this, ReceiveMessageActivity::class.java)
+         val intent = Intent(this, ReceiveMessageActivity::class.java)
         // Создать экземпляр EditView
         val messageView = findViewById<View>(R.id.message) as EditText
         // Получить строковое значения из поля text messageView
@@ -24,6 +26,21 @@ class CreateMessageActivity : AppCompatActivity() {
         // Добавить в интент информацию о тексте messageView
         intent.putExtra(ReceiveMessageActivity().EXTRA_MESSAGE, messageText)
         // Запустить активность из интента
+        startActivity(intent)
+        */
+
+        /** Вариант создания нявного интента*/
+        // Создать обхект EditText
+        val messageView = findViewById<View>(R.id.message) as EditText
+        // Получить текст из EditView
+        val messageText = messageView.text.toString()
+        // Создать неявный интент с действиенм ACTION_SEND
+        val intent = Intent(Intent.ACTION_SEND)
+        // Тип интента
+        intent.type = "text/plain"
+        // Добавление текста в интент
+        intent.putExtra(Intent.EXTRA_TEXT, messageText)
+        // Запуск активности через интент
         startActivity(intent)
     }
 }
